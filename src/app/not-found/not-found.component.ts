749115739b5e2337b7cfcc1c4f029cc4cfa630ec
garysenoc <garysenoc@gmail.com>
2021-05-03 15:08:00 +0800

    first commit

---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-Found.component.html',
+  styleUrls: ['./not-Found.component.css']
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
