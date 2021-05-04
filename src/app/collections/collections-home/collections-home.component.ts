5ce700ebc7eb45d22024c1ac80129a53bde70e2b
garysenoc <garysenoc@gmail.com>
2021-05-04 21:49:30 +0800

    asdsd

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
 
+  data = [
+    {name:'James',age:24,job:'Designer',employed:true},
+    {name:'Jill',age:26,job:'Engineer',employed:false},
+    {name:'Elyse',age:5,job:'Engineer',employed:true},
+  ];
+
+  headers = [
+{key:'employed',label:'Has a Job?'},
+{key:'name',label:'Name'},
+{key:'age',label:'Age'},
+{key:'job',label:'Job'},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mods-home',
+  templateUrl: './mods-home.component.html',
+  styleUrls: ['./mods-home.component.css']
+})
+export class ModsHomeComponent implements OnInit {
+  
+modalOpen = false;
+items=[
+  {title:"Why is the sky blue?", content:"The sky is blue because is it"},
+  {title:"What does an orange taste like", content:"An orange taste like an orange"},
+  {title:"What color is that cat?", content:"The cat is an orange color"}
+];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(){
+    this.modalOpen = !this.modalOpen;
+  }
+
+}
